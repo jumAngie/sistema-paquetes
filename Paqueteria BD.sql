eae07faa3594326a5810e41856fc130442c3461b
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2023-04-10 15:16:37 -0600

    tbPersonas

--- Paqueteria BD.sql
+++ Paqueteria BD.sql
@@ -12,3 +12,13 @@ GO
 
 
 
+CREATE TABLE Gral.tblPersonas
+(		pers_Id				INT IDENTITY(1,1),
+		pers_Nombres		NVARCHAR(250)		NOT NULL,
+		pers_Apellidos		NVARCHAR(250)		NOT NULL,
+		pers_DNI			NVARCHAR(13)		NOT NULL,
+		pers_Puesto			BIT					NOT NULL,
+		pers_Sexo			CHAR(1)				NOT NULL,
+		pers_EstadoCivil	INT					NOT NULL
+
+);
