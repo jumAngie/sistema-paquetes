8c6c17b8a6f2b6e3e1926e052aa4248bc9a0532d
Angie Campos <angie.camposyc03@gmail.com>
2023-04-17 17:50:23 -0600

    paginita de linea de tiempo

--- Paquetes-Front/flutter_paqueteria/lib/main.dart
+++ Paquetes-Front/flutter_paqueteria/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter_paqueteria/pages/index_page.dart';
 import 'package:flutter_paqueteria/pages/login_page.dart';
 import 'package:flutter_paqueteria/pages/usuarios_index_page.dart';
 import 'package:flutter_paqueteria/pages/paquetes_index_page.dart';
+import 'package:flutter_paqueteria/pages/linea_de_tiempo.dart';
 import 'package:flutter_paqueteria/pages/tracking_index_page.dart';
 import 'package:flutter_paqueteria/pages/usuario_perfil_page.dart';
 import 'package:flutter_paqueteria/pages/ejemplo.dart';
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget{
         'home': ( _ ) => HomePage(),
         'envios_index_page': ( _ ) =>  ListadoEnvios(),
         'paquetes_index_page': ( _ ) => ListadoPaquetes(),
-        'tracking_index_page': ( _ ) => Tracking(),
+        'tracking_index_page': ( _ ) => VerPaquetePage(),
         'profile_index_page': ( _ ) => Profile(),
         'login_page': ( _ ) => LoginPage()
       }
--- Paquetes-Front/flutter_paqueteria/lib/pages/linea_de_tiempo.dart
+++ Paquetes-Front/flutter_paqueteria/lib/pages/linea_de_tiempo.dart
@@ -1,18 +1,34 @@
-import 'package:timeline_tile/timeline_tile.dart';
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
-class EstadoPaquete {
-  final String estado;
-  final DateTime fecha;
-
-  EstadoPaquete({required this.estado, required this.fecha});
+class VerPaquetePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ver paquete'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.local_shipping, size: 100),
+            SizedBox(height: 20),
+            Text('Ingrese el código para ver el paquete', style: TextStyle(fontSize: 18)),
+            SizedBox(height: 20),
+            Container(
+              width: 200,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Código del paquete',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
-
-List<EstadoPaquete> estadosPaquete = [
-  EstadoPaquete(estado: 'Bodega', fecha: DateTime(2023, 4, 1)),
-  EstadoPaquete(estado: 'En Camino', fecha: DateTime(2023, 4, 5)),
-  EstadoPaquete(estado: 'Entregado', fecha: DateTime(2023, 4, 10)),
-];
 
