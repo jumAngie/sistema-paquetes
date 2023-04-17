50a1a1e0b0536ab62df11af8097a346ce3dc4c00
AlexProMaster <125411071+AlexProMaster@users.noreply.github.com>
2023-04-17 13:39:21 -0600

    Todo Unido

---
+++ Front con Cambios de Alex/Front Unido/flutter_paqueteria/lib/main.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_paqueteria/pages/home_page.dart';
+import 'package:flutter_paqueteria/pages/index_page.dart';
+import 'package:flutter_paqueteria/pages/login_page.dart';
+import 'package:flutter_paqueteria/pages/usuarios_index_page.dart';
+import 'package:flutter_paqueteria/pages/paquetes_index_page.dart';
+import 'package:flutter_paqueteria/pages/tracking_index_page.dart';
+import 'package:flutter_paqueteria/pages/usuario_perfil_page.dart';
+import 'package:flutter_paqueteria/pages/ejemplo.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({ Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Paquetería EcoPack',
+      initialRoute: LoginPage.id,
+      routes: {
+           LoginPage.id : (context)  => LoginPage(),
+
+        'home': ( _ ) => HomePage(),
+        'envios_index_page': ( _ ) =>  ListadoEnvios(),
+        'paquetes_index_page': ( _ ) => ListadoPaquetes(),
+        'tracking_index_page': ( _ ) => Tracking(),
+        'profile_index_page': ( _ ) => Profile(),
+        'login_page': ( _ ) => LoginPage()
+      }
+      
+    );
+  }
+}
